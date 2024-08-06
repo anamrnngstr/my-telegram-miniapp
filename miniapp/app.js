846464548ee9083999e07f2f4a009f6cdcd595fd
Анастасия Тюрина <anastasiaturinamac@iMac-Anastasia.local>
2024-08-07 00:19:09 +0300

    First Try

---
+++ miniapp/app.js
@@ -0,0 +1,9 @@
+document.getElementById('orderButton').addEventListener('click', function() {
+    alert('Вы выбрали "Заказать"');
+    // Здесь можно добавить код для взаимодействия с ботом через API
+});
+
+document.getElementById('performButton').addEventListener('click', function() {
+    alert('Вы выбрали "Исполнить"');
+    // Здесь можно добавить код для взаимодействия с ботом через API
+});
